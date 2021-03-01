f2f0cb1684369a43102f7887d710bf94ddfa28a0
unknown <thaivanphu.qn95@gmail.com>
2021-03-01 07:25:17 +0700

    Working on the Courses List and List Items

---
+++ src/components/courses/CourseItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <span v-for="area in areas" :key="area">{{ area }}</span>
+    </div>
+    <div class="actions">
+      <router-link :to="courseContactLink">Contact</router-link>
+      <router-link :to="courseDetailsLink">View Details</router-link>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["id", "firstName", "lastName", "rate", "areas"],
+  computed: {
+    fullName() {
+      return this.firstName + " " + this.lastName;
+    },
+    courseContactLink() {
+      return this.$route.path + "/" + this.id + "/contact"; // /courses/c1/contact
+    },
+    courseDetailsLink() {
+      return this.$route.path + "/" + this.id;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/pages/courses/CoursesList.vue
+++ src/pages/courses/CoursesList.vue
@@ -6,16 +6,26 @@
       <router-link to="/register">Đăng ký khóa học</router-link>
     </div>
     <ul v-if="hasCourses">
-      <li v-for="course in filteredCourses" :key="course.id">
-        {{ course.lastName }}
-      </li>
+      <course-item
+        v-for="course in filteredCourses"
+        :key="course.id"
+        :id="course.id"
+        :firstName="course.firstName"
+        :lastName="course.lastName"
+        :rate="course.hourlyRate"
+        :areas="course.areas"
+      ></course-item>
     </ul>
     <h3 v-else>Không tìm thấy khóa học nào.</h3>
   </section>
 </template>
 
 <script>
+import CourseItem from "../../components/courses/CourseItem";
 export default {
+  components: {
+    CourseItem,
+  },
   computed: {
     filteredCourses() {
       return this.$store.getters["courses/courses"];
@@ -26,3 +36,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
